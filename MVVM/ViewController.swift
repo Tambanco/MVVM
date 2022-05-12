50c3ed4c9da25c591f4302aef3b5f91ff48746fd
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-05-12 19:35:14 +0300

    Add project files

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by tambanco 🥳 on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
