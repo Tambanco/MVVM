db37402c73dd6512a5982d8f22ea6c678d808094
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-05-12 20:34:05 +0300

    Update MainViewModel.swift

--- MVVM/MainModule/MainViewModel.swift
+++ MVVM/MainModule/MainViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol GreetingViewModelProtocol: AnyObject {
+    var greeting: String? { get }
+    var greetingDidChange: ((GreetingViewModelProtocol) -> ())? { get set }
+    init(person: Person)
+    func showGreeting()
+}
+
+class GreetingViewModel: GreetingViewModelProtocol {
+    
+    let person: Person
+    var greeting: String?
+    
+    var greetingDidChange: ((GreetingViewModelProtocol) -> ())?
+    
+    required init(person: Person) {
+        self.person = person
+    }
+    
+    func showGreeting() {
+        self.greeting = "Hello" + " " + self.person.firstName + " " + self.person.lastName
+    }
+}
