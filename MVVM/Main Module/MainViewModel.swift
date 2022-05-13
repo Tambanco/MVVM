666fc4502f85821fb237fbf3ce20d7dbc5bb4a82
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-05-14 00:04:24 +0300

    Update MainViewModel.swift

--- MVVM/Main Module/MainViewModel.swift
+++ MVVM/Main Module/MainViewModel.swift
@@ -8,6 +8,29 @@
 import Foundation
 
 protocol MainViewModelProtocol: AnyObject {
+    
+    var greeting: String? { get }
+    var greetingDidChange: ((MainViewModelProtocol)->())? { get set }
     init(person: Person)
     func setGreeting()
+}
+
+class MainViewModel: MainViewModelProtocol {
+    
+    let person: Person
+    var greeting: String? {
+        didSet {
+            self.greetingDidChange?(self)
+        }
+    }
+    
+    var greetingDidChange: ((MainViewModelProtocol) -> ())?
+    
+    required init(person: Person) {
+        self.person = person
+    }
+    
+    func setGreeting() {
+        self.greeting = "\(person.firstName)" + "\(person.secondName)"
+    }
 }
