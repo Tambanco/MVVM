4f51c994502b90c90f65e91da8cc4fe017381a63
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-05-13 23:12:19 +0300

    Create files for MVVM module

---
+++ MVVM/Main Module/MainModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainModel.swift
+//  MVVM
+//
+//  Created by tambanco 🥳 on 13.05.2022.
+//
+
+import Foundation
---
+++ MVVM/Main Module/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  MVVM
+//
+//  Created by tambanco 🥳 on 13.05.2022.
+//
+
+import Foundation
